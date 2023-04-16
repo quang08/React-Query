6bd229ddc90331a37b160ebc655080c8ed016a17
Quang Nguyen The <quangnguyenthe@Quangs-MacBook-Air.local>
2023-04-16 20:41:00 +0700

    useQuery() basic example

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
+import { useQuery, useMutation } from "@tanstack/react-query";
+
+const POSTS = [
+  { id: 1, title: "Post 1" },
+  { id: 2, title: "Post 2" },
+];
 
 function App() {
+  const postsQuery = useQuery({
+    queryKey: ["posts"], //unique identifier for this query, a name for this query
+    queryFn: async () => {
+      //function that will be called to fetch the data
+      await wait(1000);
+      return POSTS;
+      //await Promise.reject("Error Message"); //simulate an error
+    },
+  });
+
+  if (postsQuery.isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (postsQuery.isError) {
+    return <pre>{JSON.stringify(postsQuery.error)}</pre>;
+  }
 
   return (
     <div className="App">
-      <h1>Hello </h1>
+      {postsQuery.data.map((post) => (
+        <h1 key={post.id}>{post.title}</h1>
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+function wait(duration) {
+  //manually simulate a delay in load time
+  return new Promise((resolve) => setTimeout(resolve, duration));
+}
+
+export default App;
