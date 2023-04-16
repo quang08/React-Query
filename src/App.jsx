0f571cd9478cb83f7d7852c02405db9379a63ff1
Quang Nguyen The <quangnguyenthe@Quangs-MacBook-Air.local>
2023-04-16 20:49:41 +0700

    useMutation() basic example

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useQuery, useMutation } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 
 const POSTS = [
   { id: 1, title: "Post 1" },
@@ -6,6 +6,8 @@ const POSTS = [
 ];
 
 function App() {
+  const queryClient = useQueryClient(); //basically get the query client from main.jsx so that we can invalidate the cache
+
   const postsQuery = useQuery({
     queryKey: ["posts"], //unique identifier for this query, a name for this query
     queryFn: async () => {
@@ -13,6 +15,18 @@ function App() {
       await wait(1000);
       return POSTS;
       //await Promise.reject("Error Message"); //simulate an error
+    },
+  });
+
+  const newPostMutation = useMutation({
+    mutationFn: async (title) => {
+      //function that will be called to mutate the data
+      return wait(1000).then(() => {
+        POSTS.push({ id: crypto.randomUUID(), title });
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["posts"]); //invalidate the cache for the posts query so that the new post will be fetched
     },
   });
 
@@ -29,6 +43,13 @@ function App() {
       {postsQuery.data.map((post) => (
         <h1 key={post.id}>{post.title}</h1>
       ))}
+
+      <button
+        disabled={newPostMutation.isLoading}
+        onClick={() => newPostMutation.mutate("New Post")}
+      >
+        Add New
+      </button>
     </div>
   );
 }
